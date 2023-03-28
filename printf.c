0e2c255dadbc093c9f1cc0d24f93b8027a1d51d2
JuanHoyos <hoyospiedrahitajuanmanuel@gmail.com>
2023-03-27 20:58:26 -0500

    ejercicio 5

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+    char texto[] = "\"Programming is like building a multilingual puzzle \n";
+    puts(texto);
+    return (0);
+}
